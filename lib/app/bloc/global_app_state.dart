47f6c03707f9ec6a616b8032329e1b07e98d3a46
Danila Verbinskiy <work.danv4656@gmail.com>
2024-02-10 05:16:07 +0200

    Add global app state classes and enums

--- lib/app/bloc/global_app_state.dart
+++ lib/app/bloc/global_app_state.dart
@@ -2,9 +2,70 @@ part of 'global_app_bloc.dart';
 
 sealed class GlobalAppState extends Equatable {
   const GlobalAppState();
-  
+
   @override
   List<Object> get props => [];
 }
 
-final class GlobalAppInitial extends GlobalAppState {}
+final class GlobalAppInitial extends GlobalAppState {
+  final bool isSplited = false;
+  final bool notesIsVisible = false;
+  final bool calculatorIsVisible = false;
+
+  final CalculatorModuleState calculatorModuleState =
+      CalculatorModuleState.hidden;
+  final NotesModuleState notesModuleState = NotesModuleState.hidden;
+}
+
+final class GlobalAppNotesOpened extends GlobalAppState {
+  final bool isSplited = true;
+  final bool notesIsVisible = true;
+  final bool calculatorIsVisible = false;
+
+  final CalculatorModuleState calculatorModuleState =
+      CalculatorModuleState.hidden;
+  final NotesModuleState notesModuleState = NotesModuleState.full;
+}
+
+final class GlobalAppCalculatorOpened extends GlobalAppState {
+  final bool isSplited = true;
+  final bool notesIsVisible = false;
+  final bool calculatorIsVisible = true;
+
+  final CalculatorModuleState calculatorModuleState =
+      CalculatorModuleState.full;
+  final NotesModuleState notesModuleState = NotesModuleState.hidden;
+}
+
+final class GlobalAppBothOpened extends GlobalAppState {
+  final bool isSplited = true;
+  final bool notesIsVisible = true;
+  final bool calculatorIsVisible = true;
+
+  final CalculatorModuleState calculatorModuleState = CalculatorModuleState.mid;
+  final NotesModuleState notesModuleState = NotesModuleState.mid;
+}
+
+final class GlobalAppHistoryOpened extends GlobalAppState {
+  final bool isSplited = true;
+  final bool notesIsVisible = false;
+  final bool calculatorIsVisible = true;
+
+  final CalculatorModuleState calculatorModuleState =
+      CalculatorModuleState.historyFull;
+  final NotesModuleState notesModuleState = NotesModuleState.hidden;
+}
+
+final class GlobalAppHistoryWithNotesOpened extends GlobalAppState {
+  final bool isSplited = true;
+  final bool notesIsVisible = true;
+  final bool calculatorIsVisible = true;
+
+  final CalculatorModuleState calculatorModuleState =
+      CalculatorModuleState.historyWithNotes;
+  final NotesModuleState notesModuleState = NotesModuleState.folded;
+}
+
+enum CalculatorModuleState { hidden, mid, full, historyFull, historyWithNotes }
+
+enum NotesModuleState { hidden, full, mid, folded }
