acdbd19f395ab61af938311e3d846ad7eeec75dc
Danila Verbinskiy <work.danv4656@gmail.com>
2024-02-10 22:53:59 +0200

    Update theme colors in theme.dart

--- lib/app/theme.dart
+++ lib/app/theme.dart
@@ -1,24 +1,19 @@
 import 'dart:ui';
 
-import 'package:google_fonts/google_fonts.dart';
-
 abstract final class BaseColors {
   static const Color background = Color(0xfffffffe);
-  static const Color button = const Color(0xffffd803);
+  static const Color button = Color(0xffffd803);
 
-  static const Color headerText = const Color(0xff272343);
-  static const Color inputText = const Color(0xff2D334A);
-  static Color placeholderText = Color(0xff2d334a).withOpacity(0.48);
+  static const Color headerText = Color(0xff272343);
+  static const Color inputText = Color(0xff2D334A);
+  static Color placeholderText = const Color(0xff2d334a).withOpacity(0.48);
 
-  static const Color layoutButton = const Color(0xffBAE8E8);
-  static const Color layoutButtonText = const Color(0xff272343);
+  static const Color layoutButton = Color(0xffBAE8E8);
+  static const Color layoutButtonText = Color(0xff272343);
   static Color layoutButtonActiveText =
       const Color(0xff2D334A).withOpacity(0.7);
-  static const Color layoutButtonActive = const Color(0xffE3F6F5);
-  static const Color layoutButtonActiveBorder = const Color(0xffBAE8E8);
+  static const Color layoutButtonActive = Color(0xffE3F6F5);
+  static const Color layoutButtonActiveBorder = Color(0xffBAE8E8);
 
-  static const Color textFieldBorder = const Color(0xff272343);
-}
-
-abstract final class BaseFont {
+  static const Color textFieldBorder = Color(0xff272343);
 }
